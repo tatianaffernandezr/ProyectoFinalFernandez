1553ba66187192317ce3edfda4ea96c784943c83
Tatiana Fernandez <tatianaffernandezr@hotmail.com>
2024-03-18 01:15:39 -0300

    correcciones

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
-
 (async () => {
-    const { value: email } = await Swal.fire({
+      const { value: email } = await Swal.fire({
       title: "Suscribite a nuestro Newsletter",
       input: "email",
       inputLabel: "Ingresa tu email",
@@ -9,61 +8,53 @@
     if (email) {
       Swal.fire(`Entered email: ${email}`);
     }
-  })
+})()
 
- function agregadoSesiones(){
-    setTimeout(() => {
-        document.getElementsByClassName("btn btn-outline-secondary").className= "btn btn-outline-secondary";
-        document.getElementsByClassName("btn btn-outline-secondary").innerHTML= "Sesion agregada!";
-    }, 3000);
-  }
-  document.addEventListener("click", agregadoSesiones);
-
-  const evento = () => {
-    return new Promise((resolve, reject) => {
-        resolve(2000);
-    })
-  }
-
-
-const Productos = [
-    {id:1, nombre: "Reiki Usui", Duracion:1, precio:6500},
-    {id:2, nombre: "Karmic Reiki", Duracion:1, precio:7800},
-    {id:3, nombre: "Barras de Acces", Duracion:40, precio:11000},
-    {id:4, nombre: "Gemoterapia", Duracion:1, precio:3500},
-    {id:5, nombre: "Meditacion", Duracion:2, precio:4900},
-    {id:6, nombre: "Registros Akashicos", Duracion:1, precio:12000},
+const productos = [
+    {id:1, nombre: "Reiki Usui", Duracion:1, precio:6500, categoria:"distancia"},
+    {id:2, nombre: "Karmic Reiki", Duracion:1, precio:7800, categoria: "distancia"},
+    {id:3, nombre: "Barras de Acces", Duracion:40, precio:11000, categoria: "presencial"},
+    {id:4, nombre: "Gemoterapia", Duracion:1, precio:3500, categoria: "presencial"},
+    {id:5, nombre: "Meditacion", Duracion:2, precio:4900, categoria: "distancia"},
+    {id:6, nombre: "Registros Akashicos", Duracion:1, precio:12000, categoria: "distancia"},
 ]
 
-const guardarProductosLS = (Productos) => [
-    localStorage.setItem("Productos", JSON.stringify(Productos))
-]
+const guardarCarritoLS = (productos) => {
 
-const obtenerProductosLS = () => {
-    return JSON.parse(localStorage.getItem("Productos")) || []; 
 }
-const guardarCarritoLS = (Productos) => [
-    localStorage.setItem("Carrito", JSON.stringify(Productos))
-]
-
-const obtenerCarritoLS = () => {
-    return JSON.parse(localStorage.getItem("Carrito")) || []; 
+const obtenerCarritoLS = (productos) => {
+    return JSON.parse(localStorage.getItem("carrito")) || [];
 }
-
-const totalProductos = () => {
-    const Productos = obtenerCarritoLS();
-    return Carrito.length;
+const obtenerIdProductoLS = () => {
+    return JSON.parse(localStorage.getItem("categoria")) || "";
 }
+const cantidadTotalProductos = () => {
+    const carrito = obtenerCarritoLS();
 
-function renderProductos(){
-    const Productos = obtenerProductosLS();
-    let contenido = ""
+    return carrito.length;
 }
 
 const renderTotalCarrito = () => {
-
-    document.getElementById("totalCarrito").innerHTML = totalProductos();
+    document.getElementById("totalCarrito").innerHTML = cantidadTotalProductos();
 }
 
-guardarProductosLS(Productos);
-renderProductos();
+const verProducto = (id) => {
+    localStorage.setItem("producto", JSON.stringify(id));
+}
+const verProductosPorCategoria = (id) => {
+    localStorage.setItem("categoria", JSON.stringify(id));
+}
+const obtenerProductosPorCategoria = () => {
+    const productos = obtenerProductosLS();
+    const categoria = obtenerIdPorCategoriaLS();
+    console.log(categoria);
+    const producrosFiltrados = categoria ? productos.filter(item => item.categoria === categoria) : productos;
+    
+    return producrosFiltrados;
+
+}
+const obtenerProductosLS = () => {
+    const productos = obtenerProductosLS();
+    const id = obtenerIdProductoLS();
+    const producto = productos.find(item => item.id === id);
+}
